01789fa97bb211acc2dd3dd6b97c124465fe776c
mohammadalimojtahedsoleimani <77240820+mohammadalimojtahedsoleimani@users.noreply.github.com>
2023-01-25 14:03:06 +0330

    raw landing page added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-import {Route,Routes} from "react-router-dom";
+import { Route , Routes } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Navbar from "./components/shared/Navbar";
-
+import Landing from "./components/Landing";
 
 function App () {
     return (
         <div>
 
             <Routes>
-                <Route path="/" element={<Navbar/>}/>
-                <Route path="/Signup" element={<SignUp/>}/>
-                <Route path="/Login" element={<LogIn/>}/>
+                <Route path="/" element={ <Landing/> }/>
+                <Route path="/Signup" element={ <SignUp/> }/>
+                <Route path="/Login" element={ <LogIn/> }/>
             </Routes>
-
 
 
         </div>
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./shared/Navbar";
+
+const Landing = () => {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Landing;
