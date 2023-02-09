1f2fb362da350b8a637dbbe628b8a9ba5bbe0afc
agha mamali <soleimanyali88@gmail.com>
2023-02-09 22:20:07 +0330

    post added and some tiny changes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Route , Routes } from "react-router-dom";
 import Landing from "./components/mainPages/LandingPage/Landing";
 import PostsPage from "./components/mainPages/PostsPage/PostsPage";
 import ProfilePage from './components/mainPages/ProfilePage/ProfilePage';
-
+import Base from "./components/mainPages/PostPage/Base";
 function App () {
     return (
         <div>
 
             <Routes>
                 <Route path="/" element={ <Landing/> }/>
+                <Route path="/page" element={ <Base/> }/>
                 <Route path="/Posts" element={ <PostsPage/> }/>
             </Routes>
 
