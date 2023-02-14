e82cbc9df2bf6bd9bd6a26ddf77210c6b49e39ad
amir-reza-programmer <amirrezazare130081@gmail.com>
2023-02-14 21:10:08 +0330

    fix: fixed posts size

--- src/components/mainPages/LandingPage/SelectedPosts.jsx
+++ src/components/mainPages/LandingPage/SelectedPosts.jsx
@@ -9,54 +9,49 @@ import Post from "./../../shared/Post";
 import { useContext } from "react";
 import { CharityContext } from "../../../context/CharityProvider";
 
-const SelectedPosts = ( props ) => {
-    const charity = useContext ( CharityContext );
-    const firstFiveData = charity.slice(0, 5);
+const SelectedPosts = (props) => {
+  const charity = useContext(CharityContext);
+  const firstFiveData = charity.slice(0, 5);
 
-    const set = ( posts , start , end , width ) => {
-        return posts
-            .slice ( start , end )
-            .map ( ( post ) => (
-                <Post
-                    key={ post.id }
-                    dataChar={ post }
-
-                    width={ width }
-                />
-            ) );
-    };
-    const setPosts = ( posts ) => {
-        return (
-            <div className={ styles.parent }>
-                <div className={ styles.type1_right }>{ set ( posts , 0 , 4 , 50 ) }</div>
-                <div className={ styles.type1_left }>{ set ( posts , 4 , 5 , 100 ) }</div>
-            </div>
+  const set = (posts, start, end, width, height) => {
+    return posts
+      .slice(start, end)
+      .map((post) => (
+        <Post key={post.id} dataChar={post} width={width} height={height} />
+      ));
+  };
+  const setPosts = (posts) => {
+    return (
+      <div className={styles.parent}>
+        <div className={styles.right}>{set(posts, 0, 4, 50, 15)}</div>
+        <div className={styles.left}>{set(posts, 4, 5, 100, 30)}</div>
+      </div>
+    );
+  };
+  const handlePosts = (posts) => {
+    let result = [];
+    if (posts.length - 5 < 0) {
+      return [
+        <div className={styles.parent}>
+          {setPosts(posts, 0, posts.length, 25)}
+        </div>,
+      ];
+    }
+    for (let i = 0; i < posts.length; i = i + 5) {
+      if (i + 5 <= posts.length) {
+        result.push(setPosts(posts.slice(i, i + 5)));
+      } else {
+        result.push(
+          <div className={styles.parent}>
+            {setPosts(posts, i, posts.length, 25)}
+          </div>
         );
-    };
-    const handlePosts = ( posts ) => {
-        let result = [];
-        if ( posts.length - 5 < 0 ) {
-            return [
-                <div className={ styles.parent }>
-                    { setPosts ( posts , 0 , posts.length , 25 ) }
-                </div> ,
-            ];
-        }
-        for ( let i = 0 ; i < posts.length ; i = i + 5 ) {
-            if ( i + 5 <= posts.length ) {
-                result.push ( setPosts ( posts.slice ( i , i + 5 ) ) );
-            } else {
-                result.push (
-                    <div className={ styles.parent }>
-                        { setPosts ( posts , i , posts.length , 25 ) }
-                    </div>
-                );
-                return result;
-            }
-        }
         return result;
-    };
-    return handlePosts ( firstFiveData );
+      }
+    }
+    return result;
+  };
+  return handlePosts(firstFiveData);
 };
 
 export default SelectedPosts;
--- src/components/shared/Post.jsx
+++ src/components/shared/Post.jsx
@@ -1,17 +1,31 @@
 import styles from "./Post.module.css";
 import { useNavigate } from "react-router-dom";
 
-const Post = ({ dataChar, width }) => {
-    const navigate = useNavigate();
-    const clickHandler = () =>{
-        navigate("/page");
+const Post = ({ dataChar, width, height }) => {
+  const navigate = useNavigate();
+  const clickHandler = () => {
+    navigate("/page");
+  };
+  const setHeight = () => {
+    if (width === 100) {
+      return "30vw";
     }
+    return "15vw";
+  };
   return (
-    <div style={{ width: `${width}%` }} className={styles.post} onClick={clickHandler}>
+    <div
+      style={{ width: `${width}%` }}
+      className={styles.post}
+      onClick={clickHandler}
+    >
       <div className={styles.parent}>
-        <img className={styles.image} src={dataChar.image} alt={dataChar.title}  />
+        <img
+          className={styles.image}
+          style={{ height: `${height}vw` }}
+          src={dataChar.image}
+          alt={dataChar.title}
+        />
         <h3 className={styles.title}>{dataChar.title}</h3>
-
       </div>
     </div>
   );
