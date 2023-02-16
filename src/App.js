dbe8179801432c8a5fbc382c83ede84c51d1f049
amir-reza-programmer <amirrezazare130081@gmail.com>
2023-02-16 19:11:10 +0330

    try to fix conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React , { useEffect } from 'react';
 
 import { Route , Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./components/mainPages/LandingPage/Landing";
 import PostsPage from "./pages/PostsPage";
 import { LoginModalProvider } from "./context/LoginContext";
 import { ForgetModalProvider } from "./context/forgetPassContext";
